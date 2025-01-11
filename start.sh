80352add2f20c22cb540f8c678f18cc668915dad
Edouard <edouard@7.fr>
2025-01-11 21:17:31 +0100

    feat: ajouter une boucle d'attente pour vérifier l'état du service Mattermost

--- start.sh
+++ start.sh
@@ -3,3 +3,11 @@
 echo "start.sh"
 
 /app/mattermost/bin/run
+
+NEXT_WAIT_TIME=0
+COMMAND_STATUS=1
+until [ $COMMAND_STATUS -eq 0 || $NEXT_WAIT_TIME -eq 4 ]; do
+  curl get https://chat.ospharm.ph.fr:${PORT}/api/v4/system/ping
+  sleep $NEXT_WAIT_TIME
+  let NEXT_WAIT_TIME=NEXT_WAIT_TIME+10
+done
