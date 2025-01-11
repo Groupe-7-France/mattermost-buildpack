4b683dcd9c54508a970b0630abf87d2e7e0fc983
Edouard DAUBIN <edouard@7.fr>
2025-01-11 22:30:19 +0100

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # usage: /app/mattermost/bin/run
 
-export MM_SERVICESETTINGS_LISTENADDRESS=":${PORT}"
+export MM_SERVICESETTINGS_LISTENADDRESS="127.0.0.1:8065"
 
 /app/mattermost/bin/mattermost
